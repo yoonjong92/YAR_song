d976cdd8dfed1bdd0f40143285a545ed3e3d4725
윤영준 <7057136@gmail.com>
2018-06-03 18:24:37 +0900

    add music part

---
+++ app/src/main/java/com/example/eric/yar/MusicDto.java
@@ -0,0 +1,73 @@
+package com.example.eric.yar;
+
+import java.io.Serializable;
+
+public class MusicDto implements Serializable {
+    private String id;
+    private String albumId;
+    private String title;
+    private String artist;
+    private String message;
+
+    public MusicDto() {
+    }
+
+    public MusicDto(String id, String albumId, String title, String artist) {
+        this.id = id;
+        this.albumId = albumId;
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(String albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "MusicDto{" +
+                "id='" + id + '\'' +
+                ", albumId='" + albumId + '\'' +
+                ", title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                '}';
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
